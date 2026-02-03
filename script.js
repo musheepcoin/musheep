4b10ae37dcef7b798ef179ce5cf8dcbab4c97434
orangejuicy <vincent.tranvan@gmail.com>
2026-02-03 02:57:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -80,6 +80,7 @@ window.GH_PATHS = {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ path, message: "read" }),
+      cache: "no-store", 
       keepalive: true
     });
 
